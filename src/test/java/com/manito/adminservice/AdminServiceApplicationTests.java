31b337e441efa878de3b9836fb39944786920f66
weeyoungseok <dndrldzk@gmail.com>
2026-01-03 22:28:15 +0900

    Init: setup project with Spring Boot 3.5.9 & Java 17

---
+++ src/test/java/com/manito/adminservice/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manito.adminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
